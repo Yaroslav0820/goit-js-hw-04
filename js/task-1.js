f7fd2a388dbb89b5b9add84cf374db74962a3c6c
Yaroslav Lomtev <79333072+Yaroslav0820@users.noreply.github.com>
2024-08-20 10:29:04 +0300

    Task-4

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,28 @@
-function slugify(title){
-// const titleLowe = title.toLowerCase();
-// const titleSplit = titleLowe.split(" ");
-// const titleJoin = titleSplit.join("-");
-//  return titleJoin;
- return title.toLowerCase().split(" ").join("-");
+function isEnoughCapacity(products, containerSize) {
+    let sum = 0;
+  
+    let productsArray = Object.values(products);
+    for ( const product of productsArray){
+        sum += product;
+    }
+
+    const result = sum <= containerSize;
+    return result;
+   
 }
 
-
-
-
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,22 @@
-function filterArray(numbers, value) {
-const number = [];
+const profile = {
+    username: "Jacob",
+  playTime: 300,
+  changeUsername(newName){
+  this.username = newName
+  },
+  updatePlayTime(hours){
+   this.playTime += hours
+  },
+  getInfo(){
+    return `${this.username} has ${this.playTime} active hours!`
+  },
+};
 
-for (let index = 0; index < numbers.length; index++) {
-    if ( numbers[index] > value){
-        number.push(numbers[index])
-    }
-    
-}
-return number;
-}
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
